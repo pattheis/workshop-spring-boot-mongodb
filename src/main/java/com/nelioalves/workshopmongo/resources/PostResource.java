fbea6878756f1eae1f9773ae431276035e67120d
Patrick Theis <pattrick.theis@gmail.com>
2019-08-26 01:06:46 -0300

    Obtendo um post por id

---
+++ src/main/java/com/nelioalves/workshopmongo/resources/PostResource.java
@@ -0,0 +1,27 @@
+package com.nelioalves.workshopmongo.resources;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.nelioalves.workshopmongo.domain.Post;
+import com.nelioalves.workshopmongo.services.exception.ObjectNotFoundException;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostResource repo;
+
+	public Optional<Post> findById(String id) {
+		Optional<Post> user = repo.findById(id);
+		if (user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user;
+	}
+
+}
